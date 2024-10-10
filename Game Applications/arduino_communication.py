3e0d6f15a991cce24e9d83edf7c25383faeeba72
ItaloKusmin <113540223+ItaloKusmin@users.noreply.github.com>
2024-10-09 23:09:54 -0300

    advances in arduino communication

--- Game Applications/arduino_communication.py
+++ Game Applications/arduino_communication.py
@@ -1,13 +1,16 @@
 import serial
 import pandas as pd
 import time
-import random
 
-score = 0
+trashNumber = 5 #Trashes per scenario
+trashScenarios = 1 #Current scenario
+port = 'COM5' #Port for serial communication
+baudRate = 9600 #Baud rate for serial communication
+byteNumber = 9
+    
 #Data[0] is the number of the bin which the trash was inserted (1 for organic and 2 for recyclabe);
 #Data[1:9] is the ID
-#Trash is the trash required by the game
-def trashVerify(data, trash):
+def trash_verify(data):
     trashSheet = pd.read_csv("trash.csv")
     #Saves in which bin the trash was inserted
     trashType = data[0]
@@ -16,57 +19,46 @@ def trashVerify(data, trash):
     #Verifies if the ID is in the sheet and then saves its type and name
     if id in trashSheet['Endereco'].values:
         line = trashSheet[trashSheet['Endereco'] == id]
-        type = line['Tipo'].values[0]
         name = line['Nome'].values[0]
-    else:
-        return
-    #Search in the sheet the type of the sheet based on the name required
-    if trash in trashSheet['Nome'].values:
-        line = trashSheet[trashSheet['Nome'] == trash]
+        scenario = line['Cenario'].values[0]
         if(line['Tipo'].values[0] == 'organico'):
             typeRequired = 1
         else:
             typeRequired = 2
     else:
         return
-    if(trash == name):
+    if(scenario == trashScenarios):
         if(trashType == typeRequired):
-            score += 10
+            trashNumber -= 1
             #Graphic implementation to a Right answer
             return 1
         else:
             #Graphic implementation to warn that the trash is correct, but in the wrong bin
-            score -= 5
             return 0
     else:
         #Graphic implementation to warn that it is not the trash required
-        score -= 10
         return 0
-
-if __name__ == "__main__":
-    port = 'COM5'  
-    baudRate = 9600
-    byteNumber = 9
-    #csv = pd.read_csv('trash', sep=',')
-    adresses = [151615615, 115615666]
-    types = ['organico', 'reciclavel']
-    names = ['banana', 'plastico']
-
-    csvDataFrame = {
-        'Endereco': adresses,
-        'Tipo': types,
-        'Nome': names,
-    }
-
-    trash = pd.DataFrame(csvDataFrame)
-
+    
+def send_data(servoSituation):
     ser = serial.Serial(port, baudRate)
-    while True:
-        trash
-        if ser.in_waiting >= byteNumber:
-            data = int(ser.read(byteNumber))
-            print(data)
-            feedback = trashVerify(data, )
-        else:
-            time.sleep(0.1)
+    ser.write(str(servoSituation))
+
+#This function has to be subcalled in a loop.
+def get_data():
+    ser = serial.Serial(port, baudRate)
+    if ser.in_waiting >= byteNumber:
+        data = int(ser.read(byteNumber))
+        send_data(trash_verify(data))
+    else:
+        time.sleep(0.1)
+
+#For testint
+if __name__ == "__main__":
+    while(trashScenarios < 7):
+        while(trashNumber >= 0):
+            get_data()
+        trashNumber = 5
+        trashScenarios += 1
+                    
+
         
